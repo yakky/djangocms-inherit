e9c08326f77ace597b9337bcdf6658623375882f
Iacopo Spalletti <i.spalletti@nephila.it>
2016-03-05 03:35:30 +0100

    Cleanup classifiers / README

--- README.rst
+++ README.rst
@@ -10,14 +10,11 @@ specified page (and language) in its place.
 Installation
 ------------
 
-This plugin requires `django CMS` 3.0 or higher to be properly installed.
+This plugin requires `django CMS` 3.0.4 or higher to be properly installed.
 
 1. In your projects `virtualenv`, run ``pip install djangocms-inherit``.
 2. Add ``'djangocms_inherit'`` to your ``INSTALLED_APPS`` setting.
-3. If using Django 1.6 add ``'djangocms_inherit': 'djangocms_inherit.south_migrations',``
-   to ``SOUTH_MIGRATION_MODULES``  (define ``SOUTH_MIGRATION_MODULES`` if it
-   does not already exist).
-4. Run ``manage.py migrate djangocms_inherit``.
+3. Run ``manage.py migrate djangocms_inherit``.
 
 
 ------------
--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
 from setuptools import setup
 from djangocms_inherit import __version__
+
+INSTALL_REQUIRES = [
+    'django-cms>=3.0.4'
+]
+
 
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Web Environment',
     'Framework :: Django',
+    'Framework :: Django :: 1.6',
+    'Framework :: Django :: 1.7',
+    'Framework :: Django :: 1.8',
+    'Framework :: Django :: 1.9',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
@@ -14,8 +25,12 @@ CLASSIFIERS = [
     'Topic :: Communications',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+    'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
 ]
 
 setup(
@@ -30,6 +45,7 @@ setup(
         'djangocms_inherit.migrations',
         'djangocms_inherit.south_migrations',
     ],
+    install_requires=INSTALL_REQUIRES,
     license='LICENSE.txt',
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
